e891db5d52718a4c9fdfa7924d105adc7ab723e8
Thebatman7 <rembrand.pardo@gmail.com>
2024-10-04 15:27:34 -0600

    update to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,16 +13,16 @@ const Footer = () => {
     <footer className="footer">
       <p className="copyright">© {currentYear} All rights reserved.</p>
       <div className="social-icons">
-      <a href="https://www.linkedin.com/in/rembrandpardo/" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.linkedin.com/in/rembrandpardo/" class="linkedin-icon" target="_blank" rel="noopener noreferrer">
             <Linkedin />
           </a>
-          <a href="https://github.com/Thebatman7" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/Thebatman7" class="github-icon"target="_blank" rel="noopener noreferrer">
             <GithubThick />
           </a>
-          <a href="https://www.instagram.com/rembrand.paul/" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.instagram.com/rembrand.paul/" class="instagram-icon" target="_blank" rel="noopener noreferrer">
             <Instagram />
           </a>
-          <a href="https://x.com/rembrandpardo" target="_blank" rel="noopener noreferrer">
+          <a href="https://x.com/rembrandpardo" class="x-icon"target="_blank" rel="noopener noreferrer">
             <X />
           </a>
       </div>
