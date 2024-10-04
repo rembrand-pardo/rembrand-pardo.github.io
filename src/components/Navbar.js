9bb4cb30521208be84d95912abe9df8fc733252b
Thebatman7 <rembrand.pardo@gmail.com>
2024-10-04 13:52:25 -0600

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,9 @@ import { analytics } from '../firebaseConfig';
 import { addDoc, collection } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
 
+import { cn } from '../lib/utils';
+
+
 const Navbar = ({ translations, setLanguage }) => {
   const [isVisible, setIsVisible] = useState(true);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -91,9 +94,13 @@ const Navbar = ({ translations, setLanguage }) => {
             {translations.contact}
           </Link>
           <div className="mobile-only">
+
+            {location.pathname !== '/resume' && (
             <Link to="/resume" className="resume-button" onClick={() => handleNavClick('Resume')}>
               Resume
             </Link>
+            )}
+
             <div className="mobile-language-dropdown">
               <button className="mobile-language-button">🌐</button>
               <div className="mobile-language-options">
@@ -105,11 +112,27 @@ const Navbar = ({ translations, setLanguage }) => {
           </div>
         </div>
         <div className="navbar-right-section">
+       
+        {location.pathname !== '/resume' && (
           <div className="navbar-section resume-section">
-            <Link to="/resume" className="resume-button" onClick={() => handleNavClick('Resume')}>
-              Resume
+            <Link
+              to="/resume"
+              className={cn(
+                "relative inline-flex h-8 overflow-hidden rounded-full p-[1px] focus:outline-none focus-visible:ring-2 focus-visible:ring-slate-400 focus-visible:ring-offset-2 focus-visible:ring-offset-slate-50 transition-all duration-700"
+              )}
+              onClick={() => handleNavClick('Resume')}
+            >
+              {/* Animated background effect */}
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              
+              {/* Button content */}
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl transition-colors duration-300 active:text-blue-400">
+                Resume
+              </span>
             </Link>
           </div>
+        )}
+
           <div className="navbar-section language-section">
             <button className="language-button" onClick={() => handleNavClick('Language')}>
               🌐
