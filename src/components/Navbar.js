86b30314ddb999730f91e633a97308f6d53f5d33
Thebatman7 <rembrand.pardo@gmail.com>
2024-10-07 12:33:01 -0600

    update to nav bar mobile button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -112,8 +112,17 @@ const Navbar = ({ translations, setLanguage }) => {
 
           <div className="mobile-only">
             {location.pathname !== '/resume' && (
-              <Link to="/resume" className="resume-button" onClick={() => handleNavClick('Resume')}>
-                Resume
+              <Link
+              to="/resume"
+              className={cn(
+                "relative inline-flex h-8 overflow-hidden rounded-full p-[1px] focus:outline-none focus-visible:ring-2 focus-visible:ring-slate-400 focus-visible:ring-offset-2 focus-visible:ring-offset-slate-50 transition-all duration-700"
+              )}
+              onClick={() => handleNavClick('Resume')}
+              >
+                <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl transition-colors duration-300 active:text-blue-400">
+                  Resume
+                </span>
               </Link>
             )}
             <div className="mobile-language-dropdown">
