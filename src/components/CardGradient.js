89294f463ffa57a056cc98d06c2c27b8b6004ebc
Thebatman7 <rembrand.pardo@gmail.com>
2024-12-27 14:39:40 -0700

    small change to the order of color in gradient card

--- src/components/CardGradient.js
+++ src/components/CardGradient.js
@@ -56,7 +56,7 @@ export const CardBackgroundGradient = ({
         }}
         className={cn(
           "absolute inset-0 rounded-3xl z-[1] will-change-transform",
-          "bg-[radial-gradient(circle_farthest-side_at_0_100%,#134ff5,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)]"
+          "bg-[radial-gradient(circle_farthest-side_at_0_100%,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_0,#134ff5,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)]"
         )} />
       <div className={cn("relative z-10", className)}>{children}</div>
     </div>)
