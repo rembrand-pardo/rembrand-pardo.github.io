f5494ba9d8d1a9c97682c7f85f1d11746fb37d17
Rembrand <rembrand.pardo@familysearch.org>
2025-01-05 15:19:58 -0700

    immprovements to tracingbeam text

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -147,27 +147,16 @@ const dummyContent = [
     description: (
       <>
         <p>
-          Sit duis est minim proident non nisi velit non consectetur. Esse
-          adipisicing laboris consectetur enim ipsum reprehenderit eu deserunt
-          Lorem ut aliqua anim do. Duis cupidatat qui irure cupidatat incididunt
-          incididunt enim magna id est qui sunt fugiat. Laboris do duis pariatur
-          fugiat Lorem aute sit ullamco. Qui deserunt non reprehenderit dolore
-          nisi velit exercitation Lorem qui do enim culpa. Aliqua eiusmod in
-          occaecat reprehenderit laborum nostrud fugiat voluptate do Lorem culpa
-          officia sint labore. Tempor consectetur excepteur ut fugiat veniam
-          commodo et labore dolore commodo pariatur.
+          Hello! My name is Rembrand Paul Pardo and I am from Barcelona, Spain.
+          I enjoy learning new things and finding new ways of doing things. Currently, I am studying Software Engineering at BYU.
+          Prior to this, I studied at Salt Lake Community College (SLCC) where I finished an Associate's degree in Business.
+          The world of business has always been a passion of mine because I like people and numbers.
         </p>
         <p>
-          Dolor minim irure ut Lorem proident. Ipsum do pariatur est ad ad
-          veniam in commodo id reprehenderit adipisicing. Proident duis
-          exercitation ad quis ex cupidatat cupidatat occaecat adipisicing.
-        </p>
-        <p>
-          Tempor quis dolor veniam quis dolor. Sit reprehenderit eiusmod
-          reprehenderit deserunt amet laborum consequat adipisicing officia qui
-          irure id sint adipisicing. Adipisicing fugiat aliqua nulla nostrud.
-          Amet culpa officia aliquip deserunt veniam deserunt officia
-          adipisicing aliquip proident officia sunt.
+          Software Engineering has become another passion of mine. 
+          I love how I can create new things with just lines of code. There are not limits to what programming can create.
+          I have learned that with computer-programming it is not always about what I know,
+          but what and how I can figure out what I don't know. Explore my website to find out more about what I have learned and done with programming.
         </p>
       </>
     ),
@@ -180,18 +169,11 @@ const dummyContent = [
     description: (
       <>
         <p>
-          Ex irure dolore veniam ex velit non aute nisi labore ipsum occaecat
-          deserunt cupidatat aute. Enim cillum dolor et nulla sunt exercitation
-          non voluptate qui aliquip esse tempor. Ullamco ut sunt consectetur
-          sint qui qui do do qui do. Labore laborum culpa magna reprehenderit ea
-          velit id esse adipisicing deserunt amet dolore. Ipsum occaecat veniam
-          commodo proident aliqua id ad deserunt dolor aliquip duis veniam sunt.
+          
         </p>
         <p>
-          In dolore veniam excepteur eu est et sunt velit. Ipsum sint esse
-          veniam fugiat esse qui sint ad sunt reprehenderit do qui proident
-          reprehenderit. Laborum exercitation aliqua reprehenderit ea sint
-          cillum ut mollit.
+          This is a test. We are adding more text to the description but this timer
+          it is in engilsh. We are not sure what to say here at the moment. We need improvement.
         </p>
       </>
     ),
